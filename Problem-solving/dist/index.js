4cb5c105f999db713209bf8381ebce3ec6fc9a79
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-25 15:58:10 -1000

    Task 1 fetch data function

--- Problem-solving/dist/index.js
+++ Problem-solving/dist/index.js
@@ -1,6 +1,31 @@
 "use strict";
-let testData;
-testData = ["This", "is", "a", "test", "for", "the", "TypeScript", "and", "nodemon", "configuration."];
-testData.map((data, key) => {
-    console.log(data, key);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// Function to fetch data from a Rest API endpoint via a get request
+const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
+    // Try to fetch data from rest API, if error is thrown, log the error
+    try {
+        // Send the request to the API endpoint
+        const res = yield fetch(`https://jsonplaceholder.typicode.com/photos`);
+        // If the response from the request is okay (Status code 200), set response to data variable
+        if (res.ok) {
+            const data = yield res.json();
+            // console.log(data);
+        }
+        // If response is not okay (Not status code 200), log the response status code and print something went wrong
+        else {
+            console.log(res.status, "Something went wrong with data fetching from API");
+        }
+    }
+    catch (e) {
+        console.error(e);
+    }
 });
+fetchData();
--- Problem-solving/src/index.ts
+++ Problem-solving/src/index.ts
@@ -1,7 +1,24 @@
-let testData: string[];
+// Function to fetch data from a Rest API endpoint via a get request
+const fetchData = async () => {
+  // Try to fetch data from rest API, if error is thrown, log the error
+  try {
+    // Send the request to the API endpoint
+    const res = await fetch(`https://jsonplaceholder.typicode.com/photos`);
+    // If the response from the request is okay (Status code 200), set response to data variable
+    if (res.ok) {
+      const data = await res.json();
+      // console.log(data);
+    }
+    // If response is not okay (Not status code 200), log the response status code and print something went wrong
+    else {
+      console.log(
+        res.status,
+        "Something went wrong with data fetching from API"
+      );
+    }
+  } catch (e: any) {
+    console.error(e);
+  }
+};
 
-testData = ["This", "is", "a","test","for","the","TypeScript","and","nodemon","configuration."];
-
-testData.map((data, key) => {
-  console.log(data, key);
-});
+fetchData()
