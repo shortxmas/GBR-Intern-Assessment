3a742af58722993d1518414edafced9c15c9d5f4
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-25 16:12:18 -1000

    Defined types for fetch data with interface

--- Problem-solving/dist/index.js
+++ Problem-solving/dist/index.js
@@ -10,22 +10,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 // Function to fetch data from a Rest API endpoint via a get request
 const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
-    // Try to fetch data from rest API, if error is thrown, log the error
+    // Try to fetch data from rest API, if error is thrown, log the error and return empty data array
     try {
         // Send the request to the API endpoint
         const res = yield fetch(`https://jsonplaceholder.typicode.com/photos`);
-        // If the response from the request is okay (Status code 200), set response to data variable
+        // If the response from the request is okay (Status code 200), set response data to data variable and return it
         if (res.ok) {
             const data = yield res.json();
             // console.log(data);
+            return data;
         }
-        // If response is not okay (Not status code 200), log the response status code and print something went wrong
+        // If response is not okay (Not status code 200), log the response status code, print something went wrong, return empty data array
         else {
             console.log(res.status, "Something went wrong with data fetching from API");
+            return [];
         }
     }
     catch (e) {
         console.error(e);
+        return [];
     }
 });
-fetchData();
+fetchData().then((data) => {
+    console.log(data);
+});
--- Problem-solving/src/index.ts
+++ Problem-solving/src/index.ts
@@ -1,24 +1,38 @@
+// Type interface for data being fetched, which in this case is image data
+interface Image {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
 // Function to fetch data from a Rest API endpoint via a get request
-const fetchData = async () => {
-  // Try to fetch data from rest API, if error is thrown, log the error
+const fetchData = async (): Promise<Image[]> => {
+  // Try to fetch data from rest API, if error is thrown, log the error and return empty data array
   try {
     // Send the request to the API endpoint
     const res = await fetch(`https://jsonplaceholder.typicode.com/photos`);
-    // If the response from the request is okay (Status code 200), set response to data variable
+    // If the response from the request is okay (Status code 200), set response data to data variable and return it
     if (res.ok) {
-      const data = await res.json();
+      const data: Image[] = await res.json();
       // console.log(data);
+      return data;
     }
-    // If response is not okay (Not status code 200), log the response status code and print something went wrong
+    // If response is not okay (Not status code 200), log the response status code, print something went wrong, return empty data array
     else {
       console.log(
         res.status,
         "Something went wrong with data fetching from API"
       );
+      return [];
     }
   } catch (e: any) {
     console.error(e);
+    return [];
   }
 };
 
-fetchData()
+fetchData().then((data) => {
+  console.log(data);
+});
