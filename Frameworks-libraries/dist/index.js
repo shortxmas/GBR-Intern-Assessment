ea90687d3989d0891624b6cf0588b17a4d401f64
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-26 00:08:25 -1000

    Todo post endpoint

--- Frameworks-libraries/dist/index.js
+++ Frameworks-libraries/dist/index.js
@@ -4,17 +4,44 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 // Create Express app
 const app = (0, express_1.default)();
+// Use body parser in express app
+app.use(body_parser_1.default.json());
 // Initialize mock database with an array of todo objects
 let serverData = [];
 // Define home route
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+app.get("/", (req, res) => {
+    res.status(200).send("Hello world!");
 });
 // Get all todos endpoint
-app.get('/todos', (req, res) => {
-    res.json(serverData).status(200);
+app.get("/todos", (req, res) => {
+    res.status(200).json(serverData);
+});
+// Post a new todo endpoint
+app.post("/todos", (req, res) => {
+    try {
+        const incomingTodo = req.body;
+        if (incomingTodo.description == undefined ||
+            incomingTodo.status == undefined) {
+            res
+                .status(400)
+                .send("The todo id, description or status is missing or the type todo attribute(s) type is wrong.");
+        }
+        else {
+            const newTodo = {
+                id: serverData.length + 1,
+                description: incomingTodo.description,
+                status: incomingTodo.status,
+            };
+            serverData.push(newTodo);
+            res.status(201).send("Todo has been added!");
+        }
+    }
+    catch (error) {
+        res.status(500).send("A server error has occured!");
+    }
 });
 // Start the server
 const PORT = process.env.PORT || 3000;
--- Frameworks-libraries/src/index.ts
+++ Frameworks-libraries/src/index.ts
@@ -1,27 +1,58 @@
-import express from 'express';
+import express from "express";
+import bodyParser from "body-parser";
 
 // Create Express app
 const app = express();
 
+// Use body parser in express app
+app.use(bodyParser.json());
+
 // Todo interface
-interface ToDo{
-  id:number;
-  description:string
-  status:string
+interface ToDo {
+  id: number;
+  description: string;
+  status: string;
 }
 
 // Initialize mock database with an array of todo objects
-let serverData: ToDo[] = []
+let serverData: ToDo[] = [];
 
 // Define home route
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world!");
 });
 
 // Get all todos endpoint
-app.get('/todos',(req,res)=>{
-  res.json(serverData).status(200)
-})
+app.get("/todos", (req, res) => {
+  res.status(200).json(serverData);
+});
+
+// Post a new todo endpoint
+app.post("/todos", (req, res) => {
+  try {
+    const incomingTodo: ToDo = req.body;
+    if (
+      incomingTodo.description == undefined ||
+      incomingTodo.status == undefined
+    ) {
+      res
+        .status(400)
+        .send(
+          "The todo id, description or status is missing or the type todo attribute(s) type is wrong."
+        );
+    } else {
+      const newTodo: ToDo = {
+        id: serverData.length + 1,
+        description: incomingTodo.description,
+        status: incomingTodo.status,
+      };
+      serverData.push(newTodo);
+      res.status(201).send("Todo has been added!");
+    }
+  } catch (error: any) {
+    res.status(500).send("A server error has occured!");
+  }
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
